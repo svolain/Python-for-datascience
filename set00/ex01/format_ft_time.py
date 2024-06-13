418f9bd0af58efc6f33d203eb0c55a70acf34787
Ville Savolainen <vsavolai@c3r5p2.hive.fi>
2024-06-13 16:03:59 +0300

    removing old files

--- set00/ex01/format_ft_time.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-dt = datetime.datetime.now()
-seconds = dt.timestamp()
-
-
-print('Seconds since January 1, 1970:', seconds, ' or ', "{:e}".format(seconds), ' in scientific notation')
-print (dt.strftime("%B"), dt.day, dt.year)
